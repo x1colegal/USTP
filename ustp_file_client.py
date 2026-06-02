b71f4fd3f1419bd402c3fd84603a3f7c9c45b5cd
x1colegal <x1colegal@outlook.com.br>
2026-06-02 14:19:13 -0300

    Remove deprecated USTP file transfer

--- ustp_file_client.py
+++
@@ -1,4 +0,0 @@
-print("[DEPRECATED] Insecure USTP file transfer is disabled.")
-print("Use USTP-Secure (USTPS) file transfer instead.")
-print("Repository: https://github.com/x1colegal/USTP-Secure")
-raise SystemExit(1)
